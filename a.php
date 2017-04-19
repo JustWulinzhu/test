a2753972f3e436befbc7a2ec6ea28d9438688f6c
gujinyue <gujinyue@baidu.com>
2017-04-19 16:11:13 +0800

    baidu.test

--- a.php
+++ a.php
@@ -1,9 +1,5 @@
 <?php
-	echo '1';
-	echo '2';
-	echo '3';
-	echo '4';
-	echo '5';
-	echo '6';
-    echo '7';
+    
+        echo 'this is a test file , and i will commit this file to remote repository';
+    
 ?>
