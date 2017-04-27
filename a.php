b5ddf97ddfd60d3f18e36152715cbb390e8e919f
baidu <baidu@MacBook-Pro-3.local>
2017-04-27 11:19:22 +0800

    4

--- a.php
+++ a.php
@@ -1,5 +1,23 @@
 <?php
+//冒泡排序,从小到大
+
+	function dd($arr){
+		echo "<pre>";
+			print_r($arr);
+		echo "</pre>";
+		die;
+	}
 
 	$arr = [2, 5, 9, 1, 4];
+	$count = count($arr);
+	for($i = 1; $i < $count; $i++){
+		for($j = 0; $j < $count - $i; $j++){
+			if($arr[$j + 1] < $arr[$j]){
+				$tmp = $arr[$j + 1];
+				$arr[$j + 1] = $arr[$j];
+				$arr[$j] = $tmp;
+			}
+		}
+	}
 
-
+	dd($arr);
